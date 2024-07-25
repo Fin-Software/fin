769976663e3548dc75e8af904efb5cf061504a41
Matt R Bonnette <mattrbonnette@pm.me>
2024-07-25 15:00:33 -0400

    Make vendor.sh speedy.
    Former-commit-id: eb9c9336a3c7593d215857984c56d90fa54184c5

--- vendor.sh
+++ vendor.sh
@@ -19,19 +19,19 @@ teardown()
 # Startup
 #
 
-set -eu && trap teardown INT HUP TERM EXIT
+set -eu && trap teardown INT HUP TERM EXIT && umask 0022
 readonly vendor=/tmp/vendor srcs=/tmp/srcs tball=/tmp/vendor.tar tzst=vendor.tzst
-zstd() { command zstdmt --no-check --ultra -22 "$@"; }
+zstd() { command zstdmt --no-check --ultra -22 --long=30 "$@"; }
 install() { command install -dm 0755 "$@"; }
 curl() { command curl -sL "$@"; }
 
 readonly hash=fbecc9734c5e01a45b21261a9ad58a412390a675b3e47cd0517c104f92cf0efa
-[ -f "$tzst" ] && [ "$(zstd -cd "$tzst" | b3sum)" = "$hash  -" ] && zstd -lv "$tzst" && exit 0
+[ -f "$tzst" ] && [ "$(7zz e -so "$tzst" | b3sum)" = "$hash  -" ] && zstd -lv "$tzst" && exit 0
 
 command -v gtar >/dev/null && tar() { command gtar "$@"; }
 tar --version | awk '{exit ($4 >= 1.28) ? 0 : 1}' || { printf "GNU tar too old." && exit 1; }
 
-rm -rf "$vendor" "$srcs" "$tball" "$tzst" >/dev/null && install "$vendor" "$srcs" && umask 0022
+rm -rf "$vendor" "$srcs" "$tball" "$tzst" >/dev/null && install "$vendor" "$srcs"
 
 #
 # Jobs
@@ -42,13 +42,13 @@ readonly gcc_url="https://ftp.gnu.org/gnu/gcc/gcc-$gcc_semv"
 base="gcc-$gcc_semv.tar.xz"
 {
 	[ -f "$srcs/$base" ] || curl "$gcc_url/$base" -o "$srcs/$base"
-	# TODO(@p7r0x7): Check the source's integrity and decompress with 7zz if available.
-	tar -xJf "$srcs/$base" -C "$vendor"
+	# TODO(@p7r0x7): Check the source's integrity.
+	7zz e -so "$srcs/$base" | tar -xf - -C "$vendor"
 
 	cd "$vendor/gcc-$gcc_semv" && rm -rf c++tools gotools libada libcpp libgfortran libgo libgrust libobjc libstdc++-v3
 	cd gcc && rm -rf ada cp d fortran go rust objc objcp
-	find .. -type d -name doc -exec rm -rf {} +
 	find .. -name '*ChangeLog*' -exec rm -rf {} +
+	find .. -type d -name doc -exec rm -rf {} +
 } &
 
 readonly llvm_semv=18.1.8
@@ -57,8 +57,8 @@ readonly llvm_deps="clang cmake compiler-rt libunwind lld llvm polly runtimes th
 base="llvm-project-$llvm_semv.src.tar.xz"
 {
 	[ -f "$srcs/$base" ] || curl "$llvm_url/$base" -o "$srcs/$base"
-	# TODO(@p7r0x7): Check the source's integrity and decompress with 7zz if available.
-	tar -xJf "$srcs/$base" -C "$vendor" --strip-components=1 \
+	# TODO(@p7r0x7): Check the source's integrity.
+	7zz e -so "$srcs/$base" | tar -xf - -C "$vendor" --strip-components=1 \
 		$(for dep in $llvm_deps; do printf "llvm-project-%s.src/%s\n" "$llvm_semv" "$dep"; done)
 
 	for dep in $llvm_deps; do mv "$vendor/$dep" "$vendor/$dep-$llvm_semv"; done
@@ -72,14 +72,14 @@ base="antlr-$antlr_semv-complete.jar"
 {
 	[ -f "$srcs/$base" ] || curl "$antlr_url/$base" -o "$srcs/$base"
 	# TODO(@p7r0x7): Check the source's integrity.
-	unzip -q "$srcs/$base" -d "$vendor/antlr-$antlr_semv"
+	7zz x "$srcs/$base" -o"$vendor/antlr-$antlr_semv" >/dev/null
 } &
 base="antlr4-cpp-runtime-$antlr_semv-source.zip"
 {
 	[ -f "$srcs/$base" ] || curl "$antlr_url/$base" -o "$srcs/$base"
 	# TODO(@p7r0x7): Check the source's integrity.
 	install "$vendor/antlr-cpp-runtime-$antlr_semv"
-	unzip -q "$srcs/$base" -d "$vendor/antlr-cpp-runtime-$antlr_semv"
+	7zz x "$srcs/$base" -o"$vendor/antlr-cpp-runtime-$antlr_semv" >/dev/null
 
 	rm -rf "$vendor/antlr-cpp-runtime-$antlr_semv/demo"
 } &
@@ -91,7 +91,7 @@ base="zstd-$zstd_semv.tar.zst"
 	[ -f "$srcs/$base" ] || curl "$zstd_url/$base" -o "$srcs/$base"
 	# TODO(@p7r0x7): Check the source's integrity.
 	install "$vendor/zstd-$zstd_semv"
-	zstd -cd "$srcs/$base" | tar -xf - --strip-components=1 -C "$vendor/zstd-$zstd_semv"
+	7zz e -so "$srcs/$base" | tar -xf - --strip-components=1 -C "$vendor/zstd-$zstd_semv"
 	cd "$vendor/zstd-$zstd_semv" && rm -rf contrib doc examples programs zlibWrapper lib/legacy tests
 } &
 
