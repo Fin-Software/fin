2b2b28a8665077f43faa87f5f11154a8970d25d2
Maxine R Bonnette <mattrbonnette@pm.me>
2026-04-03 11:35:42 -0400

    Mid-edit commit.

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,172 +5,154 @@
 # Contributors responsible for this file:
 # @p7r0x7 <mattrbonnette@pm.me>
 
-#set -eu; opts='-O3 -g0 -mllvm -polly -mllvm -polly-vectorizer=stripmine'
-##{ cd .build; rm -rf *.o; clang $opts -c $(find src -type file -name '*.c'); } &
-##clang $opts -c src/sha3iuf.c -o .build/c.o &
-#
-#c3c compile hash.c3 --cc "$(command -v clang)" -O0 -g0 --single-module=yes \
-#	--emit-llvm --no-obj --build-dir .build --output-dir .build || true; wait
-#
-#[ "$(uname -s)" = "Darwin" ] && gc=-dead_strip || gc=--gc-sections
-#clang $opts -fuse-ld="$(command -v ld64.lld)" -flto=thin -Wl,$gc \
-#	.build/llvm/*/direntries.ll -o .build/fin
-#exit 0
-
 supported='android/arm64 darwin/arm64 linux/arm64 linux/riscv64 linux/x64 windows/arm64 windows/x64'
 # fin also supports wasi/wasm and wasi/wasm64, but cannot be hosted on them
 
 help() {
-	[ $# = 2 ] && printf 'error: %s\n\n' "$2" >&2
-	printf '%s\n' \
-		'Usage: scripts/build.sh' \
-		'       scripts/build.sh <debug|safe> <target>' \
-		'' >&2
-	printf '       %s\n' $supported '' >&2; exit $1
+    [ $# = 2 ] && printf 'error: %s\n\n' "$2" >&2
+    printf '%s\n' \
+        'Usage: scripts/build.sh' \
+        '       scripts/build.sh <debug|safe> <target>' \
+        '' >&2
+    printf '       %s\n' $supported '' >&2; exit $1
 }
 
 settings() {
-	set -eu; umask 0022; tabs -4; install='install -dm 0755'
-	for _arg in "$@"; do case "$_arg" in -h|--help) help 0 ;; esac; done
-	case $# in
-	0)
-		_os=$(uname -s | tr '[:upper:]' '[:lower:]') _arch=$(uname -m | tr '[:upper:]' '[:lower:]')
-		case "$_arch" in x86_64|amd64) _arch=x64 ;; aarch64) _arch=arm64 ;; esac
-		[ -f /system/build.prop ] || [ -f /vendor/build.prop ] && _os=android
-		mode=safe target="$_os/$_arch" ;;
-	2)
-		mode="$1" target="$2"
-		case "$mode" in safe|debug) ;; *) help 1 'mode must be either safe or debug' ;; esac ;;
-	*)
-		help 1 'invalid argument count' ;;
-	esac
-	case "$target" in
-		android/arm64) triple=aarch64-linux-android ;;
-		darwin/arm64)  triple=aarch64-apple-macosx ;;
-		linux/arm64)   triple=aarch64-linux-gnu ;;
-		linux/riscv64) triple=riscv64-linux-gnu ;;
-		linux/x64)     triple=x86_64-linux-gnu ;;
-		windows/arm64) triple=aarch64-windows-msvc ;;
-		windows/x64)   triple=x86_64-windows-msvc ;;
-		*) help 1 "$(printf 'target %s unsupported' "$target" )" ;;
-	esac
-	target=${target%/*}-${target#*/}; $install .build/$target/build
-}
-
-c3build() {
-	true
+    set -eu; umask 0022; tabs -4; install='install -dm 0755'; $install .build
+    for _arg in "$@"; do case "$_arg" in -h|--help) help 0 ;; esac; done
+    case $# in
+    0)
+        _os=$(uname -s | tr '[:upper:]' '[:lower:]') _arch=$(uname -m | tr '[:upper:]' '[:lower:]')
+        case "$_arch" in x86_64|amd64) _arch=x64 ;; aarch64) _arch=arm64 ;; esac
+        [ -f /system/build.prop ] || [ -f /vendor/build.prop ] && _os=android
+        mode=safe target="$_os/$_arch" ;;
+    2)
+        mode="$1" target="$2"
+        case "$mode" in safe|debug) ;; *) help 1 'mode must be either safe or debug' ;; esac ;;
+    *)
+        help 1 'invalid argument count' ;;
+    esac
+    case "$target" in
+        android/arm64) triple=aarch64-linux-android ;;
+        darwin/arm64)  triple=aarch64-apple-macosx ;;
+        linux/arm64)   triple=aarch64-linux-gnu ;;
+        linux/riscv64) triple=riscv64-linux-gnu ;;
+        linux/x64)     triple=x86_64-linux-gnu ;;
+        windows/arm64) triple=aarch64-windows-msvc ;;
+        windows/x64)   triple=x86_64-windows-msvc ;;
+        *) help 1 "$(printf 'target %s unsupported' "$target" )" ;;
+    esac
+    [ ${target%/*} = darwin ] && gc=-dead_strip || gc=--gc-sections
+    case ${target%/*} in darwin) lld=ld64.lld ;; android|linux) lld=ld.lld ;; windows) lld=lld-link ;; esac
 }
 
 libs() {
-	_flags='-pipe -flto=thin -fno-omit-frame-pointer -mllvm -polly -mllvm -polly-vectorizer=stripmine'
+    _flags='-pipe -Wno-unused-command-line-argument -fno-omit-frame-pointer
+        -O3 -flto=thin -mllvm -polly -mllvm -polly-vectorizer=stripmine'
 
-	[ -f .build/hash ] || {
-		c3c compile-only hash.c3 -O0 -g0 --single-module=yes --emit-llvm --no-obj --llvm-out /tmp/fin
-		[ ${target%-*} = darwin ] && gc=-dead_strip || gc=--gc-sections
-		clang -fuse-ld="$(command -v ld64.lld)" -O3 $_flags -Wl,$gc /tmp/fin/*.ll -o .build/hash		
-	}
-	[ "$(.build/hash vendor)" = '3j8Qjuv+lRuTkMC/0HC7bEz8MYXlzVcj7AArZ8HGbF8=' ]
-	
-	case ${target%-*} in
-		darwin) set -- -DCMAKE_LINKER="$(command -v ld64.lld)" -DCMAKE_OSX_SYSROOT="$(xcrun --show-sdk-path)" ;;
-		android|linux) set -- -DCMAKE_LINKER="$(command -v ld.lld)" ;;
-		windows) set -- -DCMAKE_LINKER="$(command -v lld-link)" ;;
-	esac
-	set -- -Wno-dev -G Ninja "$@" \
-		-DCMAKE_BUILD_TYPE=Release \
-		-DCMAKE_AR="$(command -v llvm-ar)" \
-		-DCMAKE_C_COMPILER="$(command -v clang)" \
-		-DCMAKE_CXX_COMPILER="$(command -v clang++)" \
-		-DCMAKE_CXX_FLAGS_RELEASE="-O3 $_flags" \
-		-DCMAKE_C_FLAGS_RELEASE="-O3 $_flags" \
-		-DCMAKE_EXE_LINKER_FLAGS_RELEASE=-O3 \
-		-DCMAKE_OBJCOPY=false \
-		-DCMAKE_RANLIB=false \
-		-DCMAKE_NM=false
+    [ -f .build/hash ] || {
+        c3c compile-only hash.c3 -O0 -g0 --single-module=yes --emit-llvm --no-obj --llvm-out /tmp/fin
+        clang -fuse-ld="$(command -v $lld)" $_flags -Wl,$gc /tmp/fin/*.ll -o .build/hash
+    }
+    [ "$(.build/hash vendor)" = '3j8Qjuv+lRuTkMC/0HC7bEz8MYXlzVcj7AArZ8HGbF8=' ]
+    target=${target%/*}-${target#*/}; $install .build/$target/build
+    
+    set -- -Wno-dev -G Ninja \
+        -DCMAKE_BUILD_TYPE=Release \
+        -DCMAKE_AR="$(command -v llvm-ar)" \
+        -DCMAKE_LINKER="$(command -v $lld)" \
+        -DCMAKE_C_COMPILER="$(command -v clang)" \
+        -DCMAKE_CXX_COMPILER="$(command -v clang++)" \
+        -DCMAKE_EXE_LINKER_FLAGS_RELEASE=-O3 \
+        -DCMAKE_CXX_FLAGS_RELEASE="$_flags" \
+        -DCMAKE_C_FLAGS_RELEASE="$_flags" \
+        -DCMAKE_OBJCOPY=false \
+        -DCMAKE_RANLIB=false \
+        -DCMAKE_NM=false
 
-	buildzstd=.build/$target/buildzstd prefixzstd=.build/$target/installzstd
-	
-	$install $buildzstd; cmake -S vendor/zstd-*/build/cmake -B $buildzstd "$@" \
-    	-DZSTD_MULTITHREAD_SUPPORT=ON \
-    	-DZSTD_LEGACY_SUPPORT=OFF \
-    	-DZSTD_BUILD_PROGRAMS=OFF \
-    	-DZSTD_BUILD_SHARED=OFF \
-    	-DCMAKE_INSTALL_PREFIX=$prefixzstd; echo
-	
-	rm -rf $prefixzstd; ninja -C $buildzstd libzstd.a
-	ninja -C $buildzstd install >/dev/null; echo
-	
-	buildllvm=.build/$target/buildllvm prefixllvm=.build/$target/installllvm
-	
-	$install $buildllvm; cmake -S vendor/llvm-*/llvm -B $buildllvm "$@" \
-    	-DLLVM_OPTIMIZED_TABLEGEN=ON \
-    	-DLLVM_UNREACHABLE_OPTIMIZE=ON \
-    	-DLLVM_ENABLE_ASSERTIONS=OFF \
-    	-DLLVM_ENABLE_EH=OFF \
-    	-DLLVM_ENABLE_RTTI=OFF \
-    	-DLLVM_ENABLE_THREADS=ON \
-    	-DLLVM_ENABLE_LTO=Thin \
-    	-DCLANG_ENABLE_PROTO_FUZZER=OFF \
-    	-DCLANG_ENABLE_OBJC_REWRITER=OFF \
-    	-DCLANG_ENABLE_STATIC_ANALYZER=OFF \
-    	-DLLVM_ENABLE_BACKTRACES=OFF \
-    	-DLLVM_ENABLE_BINDINGS=OFF \
-    	-DLLVM_ENABLE_CRASH_OVERRIDES=OFF \
-    	-DLLVM_ENABLE_LIBEDIT=OFF \
-    	-DLLVM_ENABLE_LIBPFM=OFF \
-    	-DLLVM_ENABLE_LIBXML2=OFF \
-    	-DLLVM_ENABLE_OCAMLDOC=OFF \
-    	-DLLVM_ENABLE_PLUGINS=OFF \
-    	-DLLVM_ENABLE_Z3_SOLVER=OFF \
-    	-DLLVM_ENABLE_ZLIB=OFF \
-    	-DLLVM_ENABLE_ZSTD=ON \
-    	-DLLVM_INCLUDE_DOCS=OFF \
-    	-DLLVM_INCLUDE_TESTS=OFF \
-    	-DLLVM_INCLUDE_EXAMPLES=OFF \
-    	-DLLVM_INCLUDE_BENCHMARKS=OFF \
-    	-DCOMPILER_RT_BUILD_XRAY=OFF \
-    	-DCOMPILER_RT_BUILD_PROFILE=OFF \
-    	-DCOMPILER_RT_BUILD_BUILTINS=ON \
-    	-DCOMPILER_RT_BUILD_SANITIZERS=ON \
-    	-DLLVM_ENABLE_PROJECTS="clang;lld;polly" \
-    	-DLLVM_ENABLE_RUNTIMES="compiler-rt;openmp" \
-    	-DLLVM_TARGETS_TO_BUILD="X86;AArch64;ARM;RISCV;WebAssembly;LoongArch" \
-    	-DCMAKE_PREFIX_PATH=$prefixzstd \
-    	-DCMAKE_INSTALL_PREFIX=$prefixllvm; echo
-	
-	rm -rf $prefixllvm; ninja -C $buildllvm \
-    	install-clangDriver \
-    	install-lldCOFF \
-    	install-lldCommon \
-    	install-lldELF \
-    	install-lldMachO \
-    	install-lldWasm \
-    	install-LLVMFrontendOpenMP \
-    	install-LLVMOrcJIT \
-    	install-Polly
-	ninja -C $buildllvm install-llvm-headers install-clang-headers install-lld-headers >/dev/null; echo
-	
-	{
-    	cd $prefixllvm/include/lld; $install COFF ELF MachO wasm; cd ../../../..
-    	for dir in COFF ELF MachO wasm; do ln $buildllvm/tools/lld/$dir/Options.inc $prefixllvm/include/lld/$dir; done
-    	ln $buildllvm/tools/lld/include/lld/Common/Version.inc $prefixllvm/include/lld/Common
-    	rm -rf .build/include .build/libzstd.a; mv $prefixllvm/include .build/include; mv $prefixzstd/include .build/include/zstd
-    	mv $prefixzstd/lib/libzstd.a .build; rm -rf $prefixzstd &
-	} &
-	{
-    	cd $prefixllvm/lib
-    	for lib in *.a; do { dir=${lib%%.a}; $install $dir; cd $dir; llvm-ar -x ../$lib; } & done; wait; rm -- *.a
-    	for dir in $(echo * | LC_ALL=C sort); do { du -csh $(echo $dir/* | LC_ALL=C sort); echo; } done
-    	set -- $(echo */* | LC_ALL=C sort); [ -e ../../libLLVM.a ] && rm ../../libLLVM.a
-    	llvm-ar rcs ../../libLLVM.a "$@"; rm -rf ../../installllvm &
-    	printf -- '--------------------------------------------------------\n %s    objects archived   %s    total bytes\n' \
-        	$# "$(wc -c < ../../libLLVM.a)"
-	} &
-	wait
+    buildzstd=.build/$target/build/zstd prefixzstd=.build/$target/install/zstd
+    
+    $install $buildzstd; cmake -S vendor/zstd-*/build/cmake -B $buildzstd "$@" \
+        -DZSTD_MULTITHREAD_SUPPORT=ON \
+        -DZSTD_LEGACY_SUPPORT=OFF \
+        -DZSTD_BUILD_PROGRAMS=OFF \
+        -DZSTD_BUILD_SHARED=OFF \
+        -DCMAKE_INSTALL_PREFIX=$prefixzstd; echo
+    
+    rm -rf $prefixzstd; ninja -C $buildzstd libzstd.a
+    ninja -C $buildzstd install >/dev/null; echo
+    
+    buildllvm=.build/$target/build/llvm prefixllvm=.build/$target/install/llvm
+    
+    $install $buildllvm; cmake -S vendor/llvm-*/llvm -B $buildllvm "$@" \
+        -DLLVM_OPTIMIZED_TABLEGEN=ON \
+        -DLLVM_UNREACHABLE_OPTIMIZE=ON \
+        -DLLVM_ENABLE_ASSERTIONS=OFF \
+        -DLLVM_ENABLE_EH=OFF \
+        -DLLVM_ENABLE_RTTI=OFF \
+        -DLLVM_ENABLE_THREADS=ON \
+        -DLLVM_ENABLE_LTO=Thin \
+        -DCLANG_ENABLE_PROTO_FUZZER=OFF \
+        -DCLANG_ENABLE_OBJC_REWRITER=OFF \
+        -DCLANG_ENABLE_STATIC_ANALYZER=OFF \
+        -DLLVM_ENABLE_BACKTRACES=OFF \
+        -DLLVM_ENABLE_BINDINGS=OFF \
+        -DLLVM_ENABLE_CRASH_OVERRIDES=OFF \
+        -DLLVM_ENABLE_LIBEDIT=OFF \
+        -DLLVM_ENABLE_LIBPFM=OFF \
+        -DLLVM_ENABLE_LIBXML2=OFF \
+        -DLLVM_ENABLE_OCAMLDOC=OFF \
+        -DLLVM_ENABLE_PLUGINS=OFF \
+        -DLLVM_ENABLE_Z3_SOLVER=OFF \
+        -DLLVM_ENABLE_ZLIB=OFF \
+        -DLLVM_ENABLE_ZSTD=ON \
+        -DLLVM_INCLUDE_DOCS=OFF \
+        -DLLVM_INCLUDE_TESTS=OFF \
+        -DLLVM_INCLUDE_EXAMPLES=OFF \
+        -DLLVM_INCLUDE_BENCHMARKS=OFF \
+        -DCOMPILER_RT_BUILD_XRAY=OFF \
+        -DCOMPILER_RT_BUILD_PROFILE=ON \
+        -DCOMPILER_RT_BUILD_BUILTINS=ON \
+        -DCOMPILER_RT_BUILD_SANITIZERS=ON \
+        -DLLVM_ENABLE_PROJECTS="clang;lld;polly" \
+        -DLLVM_ENABLE_RUNTIMES="compiler-rt;openmp" \
+        -DLLVM_TARGETS_TO_BUILD="X86;AArch64;ARM;RISCV;WebAssembly;LoongArch" \
+        -DCMAKE_PREFIX_PATH=$prefixzstd \
+        -DCMAKE_INSTALL_PREFIX=$prefixllvm; echo
+    
+    rm -rf $prefixllvm; ninja -C $buildllvm \
+        install-clangDriver \
+        install-lldCOFF \
+        install-lldCommon \
+        install-lldELF \
+        install-lldMachO \
+        install-lldWasm \
+        install-LLVMFrontendOpenMP \
+        install-LLVMOrcJIT \
+        install-Polly
+    ninja -C $buildllvm install-llvm-headers install-clang-headers install-lld-headers >/dev/null; echo
+    
+    #{
+    #	cd $prefixllvm/include/lld; $install COFF ELF MachO wasm; cd ../../../..
+    #	for dir in COFF ELF MachO wasm; do ln $buildllvm/tools/lld/$dir/Options.inc $prefixllvm/include/lld/$dir; done
+    #	ln $buildllvm/tools/lld/include/lld/Common/Version.inc $prefixllvm/include/lld/Common
+    #	rm -rf .build/include .build/libzstd.a; mv $prefixllvm/include .build/include; mv $prefixzstd/include .build/include/zstd
+    #	mv $prefixzstd/lib/libzstd.a .build; rm -rf $prefixzstd &
+    #} &
+    #{
+    #	cd $prefixllvm/lib
+    #	for lib in *.a; do { dir=${lib%%.a}; $install $dir; cd $dir; llvm-ar -x ../$lib; } & done; wait; rm -- *.a
+    #	for dir in $(echo * | LC_ALL=C sort); do { du -csh $(echo $dir/* | LC_ALL=C sort); echo; } done
+    #	set -- $(echo */* | LC_ALL=C sort); [ -e ../../libLLVM.a ] && rm ../../libLLVM.a
+    #	llvm-ar rcs ../../libLLVM.a "$@"; rm -rf ../../installllvm &
+    #	printf -- '--------------------------------------------------------\n %s    objects archived   %s    total bytes\n' \
+    #		$# "$(wc -c < ../../libLLVM.a)"
+    #} &
+    #wait
 }
 
 build() {
-	true
+    true
 }
 
 settings "$@"; libs; build
--- scripts/vendor.sh
+++ scripts/vendor.sh
@@ -9,56 +9,68 @@
 # And for security reasons, only @p7r0x7 may sign and push commits changing vendor.sh and vendor/.
 # I refactor this mostly for the joy of programming, but now it's hyper optimized.
 
-szip() { m=$1; shift; $zip $m -bd -ssc "$@"; }
 command -v 7zz >/dev/null && zip=7zz || zip=7z
 command -v gfind >/dev/null && find=gfind || find=find
 srcs=/tmp/fin/srcs install='install -dm 0755' rm='rm -rf --'
-set -e; umask 0022; [ $# = 0 ] || return 0  # . ends
+set -eu; umask 0022; [ $# = 0 ] || return 0  # . ends
+szip() { _mode=$1; shift; $zip $_mode -bd -ssc "$@"; }
 
-start() { name=$1; . ../scripts/vendor.sh; pkg=/tmp/fin/$name; $rm $pkg; $install $pkg; }  # . borrows $@
+start() { _name=$1; . ../scripts/vendor.sh; pkg=/tmp/fin/$_name; $rm $pkg; $install $pkg; }  # . borrows $@
 get() {
-    hash=$1; [ -f $srcs/$base ] || curl --fail-with-body -sSL $url -o $srcs/$base
-    real="$(szip h -scrcSHA3-256 $srcs/$base | awk '/^SHA3-256 for/ {print $4}')"
-    [ "$real" = $hash ] && return 0 || curl --fail-with-body -sSL $url -o $srcs/$base
-    real="$(szip h -scrcSHA3-256 $srcs/$base | awk '/^SHA3-256 for/ {print $4}')"
-    [ "$real" = $hash ] || { printf 'get %s !=\n%s\n' $hash "$real"; return 1; }
+    _hash=$1; [ -f $srcs/$base ] || curl --fail-with-body -sSL https://$url -o $srcs/$base
+    _real="$(szip h -scrcSHA3-256 $srcs/$base | awk '/^SHA3-256 for/ {print $4}')"
+    [ "$_real" = $_hash ] && return 0 || curl --fail-with-body -sSL https://$url -o $srcs/$base
+    _real="$(szip h -scrcSHA3-256 $srcs/$base | awk '/^SHA3-256 for/ {print $4}')"
+    [ "$_real" = $_hash ] || { printf 'get %s !=\n%s\n' $_hash "$_real"; return 1; }
 }
-finish() { $find $pkg -type d -empty -delete; for dir in $pkg/*; do $rm ${dir##*/}; done; mv $pkg/* .; rmdir $pkg; }
+finish() {
+    $find $pkg -type d -empty -delete; for _dir in $pkg/*; do $rm ${_dir##*/}; done; mv $pkg/* .; rmdir $pkg
+}
 
 #
 #    Separate collections of parallelly-acquiesced dependencies.
 #
 llvm() {
     start llvm; semv=22.1.2 base=llvm-project-$semv.src.tar.xz
+    url=github.com/llvm/llvm-project/releases/download/llvmorg-$semv/$base
     deps='clang cmake compiler-rt lld llvm openmp polly runtimes third-party'
-    url=https://github.com/llvm/llvm-project/releases/download/llvmorg-$semv/$base
     (
-        get e25036d460357ec4c542d5507feceb8a21d23f98f86234291a902392a8bcde07
-        set -f; szip e -so $srcs/$base | szip x -o$pkg -si -ttar $(printf -- '-x!*/%s ' */bindings */docs       \
-            */examples */test */unittests */www llvm/benchmarks polly/lib/External/isl/test_inputs) $(printf -- \
-            '-xr!%s ' .* Maintainers.* CREDITS.* *.png *.bmp) $(printf '*/%s ' $deps) >/dev/null; set +f
+        get e25036d460357ec4c542d5507feceb8a21d23f98f86234291a902392a8bcde07; set -f
+            szip e -so $srcs/$base | szip x -o$pkg -si -ttar $(printf -- '-x!*/%s ' */bindings */docs */www */examples \
+            */test */unittests llvm/benchmarks polly/lib/External/isl/test_inputs) $(printf --  \
+            '-xr!%s ' .* Maintainers.* CREDITS.* *.png *.bmp) $(printf -- '*/%s ' $deps) >/dev/null
+            set +f
 
         cd $pkg; mv llvm-project-$semv.src llvm-$semv; sed -i '' '982,986d' llvm-$semv/llvm/CMakeLists.txt
-        guard='s|^[[:space:]]*add_subdirectory[[:space:]]*\(([^)]+)\)|if(EXISTS  '
-        guard=$guard'"${CMAKE_CURRENT_SOURCE_DIR}/\1")\n  add_subdirectory(\1)\nendif()|'
-        find . -name CMakeLists.txt -exec sed -i '' -E "$guard" {} +
+        _guard='s|^[[:space:]]*add_subdirectory[[:space:]]*\(([^)]+)\)|if(EXISTS  '
+        _guard=$_guard'"${CMAKE_CURRENT_SOURCE_DIR}/\1")\n  add_subdirectory(\1)\nendif()|'
+        $find . -name CMakeLists.txt -exec sed -i '' -E "$_guard" {} +
     ); finish
 }
 other() {
-    start other; semv=master base=LRSTAR-$semv.tar.gz url=https://github.com/p7r0x7/LRSTAR/archive/$semv.tar.gz
+    start other; semv=master base=LRSTAR-$semv.tar.gz url=github.com/p7r0x7/LRSTAR/archive/${base#*-}
     (
-        get f6b33c1ea42ebdd1793728daa82a30095848345958b9498b3fbb6e9da00534a2
-        set -f; szip e -so $srcs/$base | szip x -o$pkg -si -ttar \
-            -x!'*/bin' -xr!'?G.*.txt' $(printf '*/*/*.%s ' h hpp cpp txt grm lgr) >/dev/null; set +f
+        get f6b33c1ea42ebdd1793728daa82a30095848345958b9498b3fbb6e9da00534a2; set -f
+            szip e -so $srcs/$base | szip x -o$pkg -si -ttar $(printf -- '-i!*/*/*.%s ' h hpp cpp txt grm lgr) \
+            -x!*/bin -xr!?G.*.txt >/dev/null
+            set +f
         cd $pkg; mv LRSTAR-$semv lrstar-$semv
     ) &
 
-    semv=1.5.7 base=zstd-$semv.tar.zst url=https://github.com/facebook/zstd/releases/download/v$semv/$base
+    semv=1.5.7 base=zstd-$semv.tar.zst url=github.com/facebook/zstd/releases/download/v$semv/$base
     (
-        get faf88aa26fa06f469e05fcb48434804905f70eb36c7760916f2f4de4287c85f6
-        set -f; szip e -so $srcs/$base | szip x -o$pkg -si -ttar $(printf -- '-x!*/%s ' zlibWrapper \
-            lib/deprecated lib/legacy build/meson build/VS* *T*.md Package.swift CHANGELOG)         \
-            $(printf -- '-xr!%s ' contrib/ example*/ programs/ tests/ *.png .*) >/dev/null; set +f
+        get faf88aa26fa06f469e05fcb48434804905f70eb36c7760916f2f4de4287c85f6; set -f
+            szip e -so $srcs/$base | szip x -o$pkg -si -ttar $(printf -- '-x!*/%s ' zlibWrapper lib/deprecated \
+            lib/legacy build/meson build/VS* *T*.md Package.swift CHANGELOG)            \
+            $(printf -- '-xr!%s ' contrib/ example*/ programs/ tests/ *.png .*) >/dev/null
+            set +f
+    ) &
+
+    semv=1.3.2 base=zlib-$semv.tar.xz url=github.com/madler/zlib/releases/download/v$semv/$base
+    (
+        get d60ffcfad05908d1efb932177340d2c80c9db123cf9eadff5657945f214a2214; set -f
+            szip e -so $srcs/$base | szip x -o$pkg -si -ttar $(printf -- '-i!*%s ' c h in) -i!CMakeLists.txt >/dev/null
+            set +f
     ) & wait; finish
 }
 
