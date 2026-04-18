c58f1d37b7cec239d76014e15db53eeaf8bdc415
Maxine R Bonnette <mattrbonnette@pm.me>
2026-04-18 17:58:49 -0400

    We have a parser again! Many design updates.

--- scripts/repo.sh
+++ scripts/repo.sh
@@ -5,22 +5,25 @@
 # Contributors responsible for this file:
 # @p7r0x7 <mattrbonnette@pm.me>
 
+#    Does shit.
+
 set -eu; umask 0022; rm -rf src/lrstar; install -dm 0755 src/lrstar .build
 
-opt="-O3 -mllvm -polly -mllvm -polly-vectorizer=stripmine"
+opt='-pipe -O3 -flto=thin -mllvm -polly -mllvm -polly-vectorizer=stripmine -fno-omit-frame-pointer'
 [ -f .build/lrstar ] || clang $opt -w -lc++ -o .build/lrstar vendor/lrstar-master/source_lrstar/*.cpp &
 [ -f .build/dfa ] || clang $opt -w -lc++ -o .build/dfa vendor/lrstar-master/source_dfa/*.cpp & wait
 
 cd src/lrstar; ln ../Fin.grm Fin.grm; ln ../Fin.lgr Fin.lgr
-{ ../../.build/lrstar Fin.grm /crr /csr /o; echo; ../../.build/dfa Fin.lgr /crr /csr /sto; } || true
-rm -f -- *.grm *.lex *.lgr *grammar.txt *states.txt make.bat memory.txt
-cd ../..
 
-#find . \( -path ./vendor -o -name '.[!.]*' \) -prune -o \
-#    \( ! -type d \( \
-#        -name '*.zig' -exec zig fmt {} + -o \
-#        \( -name '*.c' -o -name '*.cpp' -o -name '*.h' -o -name '*.hpp' \) \
-#            -exec clang-format -i --style file:.clang-format {} + \
-#    \) \)
+printf '\033[1;33m'; { ../../.build/lrstar Fin.grm /crr /csr /k=2 /o; echo
+  ../../.build/dfa Fin.lgr /crr /csr /sto; } || true; printf '\033[0m\n'
 
-git add .; git diff --stat HEAD; git reset
+rm -f -- *.grm *.lex *.lgr *grammar.txt *states.txt make.bat memory.txt; cd ../..
+
+find . \( -path ./vendor -o -name '.[!.]*' \) -prune -o \
+    \( ! -type d \( \
+        \( -name '*.c' -o -name '*.cpp' -o -name '*.h' -o -name '*.hpp' \) \
+            -exec clang-format -i --style file:.clang-format {} + \
+    \) \)
+
+git add .; git --no-pager diff --stat HEAD; git reset >/dev/null
