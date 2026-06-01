2a1afc864dda4d6225f3e1ffc6c845bd97f979f5
Maxine R Bonnette <mattrbonnette@pm.me>
2026-05-31 20:08:29 -0400

    Project structure refactor.

--- scripts/repo.sh
+++ scripts/repo.sh
@@ -7,7 +7,7 @@
 
 #    Does shit.
 
-set -eu; umask 0022; install -dm 0755 src/lrstar .build
+set -eu; umask 0022; install -dm 0755 src/frontend/lrstar .build
 
 # Build LRSTAR
 opt='-pipe -O3 -mllvm -polly -mllvm -polly-vectorizer=stripmine -fno-omit-frame-pointer'
@@ -15,10 +15,10 @@ opt='-pipe -O3 -mllvm -polly -mllvm -polly-vectorizer=stripmine -fno-omit-frame-
 [ -f .build/dfa ] || clang++ $opt -w -lc++ -o .build/dfa vendor/lrstar-master/source_dfa/*.cpp & wait
 
 # Run LRSTAR
-cd src/lrstar; ln ../Fin.grm Fin.grm; ln ../Fin.lgr Fin.lgr
-printf '\033[1;33m'; { ../../.build/lrstar Fin.grm /crr /csr /wk /k=2 /o /m
-    echo; ../../.build/dfa Fin.lgr /crr /csr /sto /m; } || true
-rm -f -- *.grm *.lgr *.lex *grammar.txt *log.txt make.bat memory.txt; cd ../..
+cd src/frontend/lrstar; ln ../Fin.grm Fin.grm; ln ../Fin.lgr Fin.lgr
+printf '\033[1;33m'; { ../../../.build/lrstar Fin.grm /crr /csr /wk /k=2 /o /m
+    echo; ../../../.build/dfa Fin.lgr /crr /csr /sto /m; } || true
+rm -f -- *.grm *.lgr *.lex *grammar.txt *states.txt *log.txt make.bat memory.txt; cd ../../..
 
 # Normalize repo
 find . \( -path ./vendor -o -path '*/.*' \) -prune -o \
@@ -38,8 +38,7 @@ find . \( -path ./vendor -o -path '*/.*' \) -prune -o \
 chmod a+x scripts/*sh
 
 # Run Lexer+Parser
-rm code 2>/dev/null || true; ln -s vendor/lrstar-master/code code
-clang++ -O1 -w -lc++ -include sys/stat.h -o .build/finlp src/lrstar/*.cpp
+clang++ -O0 -w -lc++ -include sys/stat.h -Ivendor/lrstar-master -o .build/finlp src/frontend/lrstar/*.cpp
 mawk '
 function spaces(n, gap) {
     if ((gap = n-length(SPACES)) > 0) SPACES = SPACES sprintf("%*s", gap, "")
@@ -76,8 +75,8 @@ function whiteoutnontab(s, out, parts, n, i) {
 }
 ' research/finfile.fn >research/finfile.wo.fn
 sz1=$(wc -c <research/finfile.fn) sz2=$(wc -c <research/finfile.wo.fn); [ $sz1 -eq $sz2 ] || [ $sz1 -eq $((sz2 - 1)) ]
-.build/finlp research/finfile.wo.fn || true;  printf '\033[0m\n'
-rm code research/finfile.wo.fn lrstar.txt 2>/dev/null || true
+.build/finlp research/finfile.wo.fn || true; printf '\033[0m\n'
+rm research/finfile.wo.fn lrstar.txt research/finfile.output.txt 2>/dev/null || true
 
 # Print git status
 (GIT_INDEX_FILE=.git/index.tmp; export GIT_INDEX_FILE; git read-tree HEAD; git add .; git --no-pager diff --stat HEAD)
